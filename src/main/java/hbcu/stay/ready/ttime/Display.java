ec5a429911079bd129f0cabd66c8ad8c95a90fd3
admin <admin@Briannas-MacBook-Pro.local>
2019-07-24 10:11:20 -0400

    made changes to display

--- src/main/java/hbcu/stay/ready/ttime/Display.java
+++ src/main/java/hbcu/stay/ready/ttime/Display.java
@@ -2,9 +2,9 @@ package hbcu.stay.ready.ttime;
 
 public class Display extends Calculator {
    String[] switchDisplayMode = {"binary","octal","decimal","hexadecimal"};
+   String displayMode = "binary";
+   String unitsMode = "degree";
    int i = 0;
-   String displayMode = switchDisplayMode[i];
-
    public void switchDisplayModes(){
       i++;
       if(i == 4){
@@ -15,4 +15,12 @@ public class Display extends Calculator {
    public void switchDisplayModes(String mode){
       displayMode = mode;
 }
+   public void switchUnitsMode(){
+      if(unitsMode == "Degrees"){
+         unitsMode = "Radians";
+      }
+      else{
+         unitsMode = "Degrees";
+      }
+   }
 }
